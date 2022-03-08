296e4bb6f7a8cee7a2f62b8b6fa17c81576f353a
Victor Lobe Jr <viclobejr@gmail.com>
2022-03-08 10:36:49 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+MinecraftAPI.getServerStatus('play.totalfreedom.me', {
+    port: 25565
+}, function (err, status) {
+
+    var s = document.querySelector('#server-status');
+    var p = document.querySelector('#player-count');
+
+    if (err) {
+
+        s.innerHTML = 'offline';
+        p.innerHTML = '0';
+        return q.classList.add('red-text')
+
+    }
+
+    if (status.online) {
+
+        s.innerHTML = "online";
+        p.innerHTML = `${status.players.now}/${status.players.max}`;
+        s.classList.add('green-text');
+        return p.classList.add('green-text');
+
+    } else {
+
+        s.innerHTML = 'Offline';
+        p.innerHTML = '0';
+        return s.classList.add('red-text');
+
+    }
+});
