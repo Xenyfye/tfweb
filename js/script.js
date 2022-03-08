3a286637738d17db5fec820652c986206abb4217
Victor Lobe Jr <viclobejr@gmail.com>
2022-03-08 16:27:52 -0700

    Fixed bug & better formatted code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 MinecraftAPI.getServerStatus('play.totalfreedom.me', {
-    port: 25565
+	port: 25565
 }, function (err, status) {
 
-    var s = document.querySelector('#server-status');
-    var p = document.querySelector('#player-count');
+	var s = document.querySelector('#server-status');
+	var p = document.querySelector('#player-count');
 
-    if (err) {
+	if (err) {
 
-        s.innerHTML = 'offline';
-        p.innerHTML = '0';
-        return q.classList.add('red-text')
+		s.innerHTML = 'offline';
+		p.innerHTML = '0';
+		return q.classList.add('red-text')
 
-    }
+	}
 
-    if (status.online) {
+	if (status.online) {
 
-        s.innerHTML = "online";
-        p.innerHTML = `${status.players.now}/${status.players.max}`;
-        s.classList.add('green-text');
-        return p.classList.add('green-text');
+		s.innerHTML = "online";
+		p.innerHTML = `${status.players.now}/${status.players.max}`;
+		s.classList.add('green-text');
+		return p.classList.add('green-text');
 
-    } else {
+	} else {
 
-        s.innerHTML = 'Offline';
-        p.innerHTML = '0';
-        return s.classList.add('red-text');
+		s.innerHTML = 'Offline';
+		p.innerHTML = '0';
+		return s.classList.add('red-text');
 
-    }
+	}
 });
